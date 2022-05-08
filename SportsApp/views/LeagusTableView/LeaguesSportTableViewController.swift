f6f41c2c1763be51a9aa7b728d9c8b494134ecac
raiaan <raiaanm@gmail.com>
2022-05-08 21:58:02 +0200

    adding leagues detail to navigation

--- SportsApp/views/LeagusTableView/LeaguesSportTableViewController.swift
+++ SportsApp/views/LeagusTableView/LeaguesSportTableViewController.swift
@@ -26,6 +26,16 @@ class LeaguesSportTableViewController: UITableViewController {
         cell.leagueBadge.sd_setImage(with: URL(string: models[indexPath.row].strBadge) )
         return cell
     }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if currentReachabilityStatus == .notReachable {
+            showDialoug()
+        } else {
+            var LegueDetail = self.storyboard?.instantiateViewController(withIdentifier: "LeaguesDetailsViewController") as! LeaguesDetailsViewController
+            LegueDetail.league = models[indexPath.row]
+            LegueDetail.shouldUpdateView = true
+            self.navigationController?.pushViewController(LegueDetail, animated: true)
+        }
+    }
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 75
     }
@@ -40,5 +50,10 @@ class LeaguesSportTableViewController: UITableViewController {
             self.models = leagusViewModels.league ?? [Leagus]()
             self.tableView.reloadData()
         }
+    }
+    func showDialoug(){
+        let alert = UIAlertController (title: "No Connection!", message: "sorry there's no Internet Connection to Display Leagues details", preferredStyle: UIAlertController.Style.alert )
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
